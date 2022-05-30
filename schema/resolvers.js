c7c4d1a7e262e3363422e41563cfe1d69772be06
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2022-05-30 23:22:41 +0800

    Including Movie Types and Resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,13 +1,21 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
 
 const resolvers = {
   Query: {
+    //User Resolvers
     users: () => {
       return UserList;
     },
     user: (_, args) => {
       const id = args.id;
       return UserList.find((user) => user.id === +id);
+    },
+
+    //Movie Resolvers
+    movies: () => MovieList,
+    movie: (_, args) => {
+      const name = args.name;
+      return MovieList.find((movie) => movie.name === name);
     },
   },
 };
